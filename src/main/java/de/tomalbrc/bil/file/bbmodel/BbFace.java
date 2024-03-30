e6c923a0aa23e7b66418cce81423b0701351462d
tomalbrc <tomalbrc@me.com>
2024-03-30 22:58:31 +0100

    texture rotation fix

--- src/main/java/de/tomalbrc/bil/file/bbmodel/BbFace.java
+++ src/main/java/de/tomalbrc/bil/file/bbmodel/BbFace.java
@@ -6,5 +6,7 @@ public class BbFace {
     public List<Float> uv;
     public int texture = 0;
     public int tintindex = 0;
+
+    public int rotation;
     public String cullface;
 }
--- src/main/java/de/tomalbrc/bil/json/FaceSerializer.java
+++ src/main/java/de/tomalbrc/bil/json/FaceSerializer.java
@@ -17,6 +17,7 @@ public class FaceSerializer implements JsonSerializer<BbFace> {
         obj.addProperty("texture", String.format("#%d", src.texture));
         if (src.cullface != null) obj.addProperty("cullface", src.cullface);
         obj.addProperty("tintindex", 0);
+        if (src.rotation != 0) obj.addProperty("rotation", src.rotation);
         obj.add("uv", context.serialize(src.uv));
 
         return obj;

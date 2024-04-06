3f527d49dd0f27e593ec8d640ccfdfa798622f52
tomalbrc <tomalbrc@me.com>
2024-04-06 22:14:51 +0200

    cleanup

--- src/main/java/de/tomalbrc/bil/BIL.java
+++ src/main/java/de/tomalbrc/bil/BIL.java
@@ -1,20 +1,13 @@
 package de.tomalbrc.bil;
 
-import com.google.common.base.Stopwatch;
 import com.mojang.logging.LogUtils;
 import de.tomalbrc.bil.command.BILCommand;
-import de.tomalbrc.bil.core.model.Model;
-import de.tomalbrc.bil.file.extra.interpolation.CatmullRomInterpolator;
-import de.tomalbrc.bil.file.loader.BbModelLoader;
 import de.tomalbrc.bil.util.RPUtil;
 import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import gg.moonflower.molangcompiler.api.MolangCompiler;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import org.joml.Vector3f;
 import org.slf4j.Logger;
-
-import java.util.concurrent.TimeUnit;
 
 public class BIL implements ModInitializer {
     public static MolangCompiler COMPILER = MolangCompiler.create(MolangCompiler.DEFAULT_FLAGS, BIL.class.getClassLoader());

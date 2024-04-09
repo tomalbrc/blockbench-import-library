9a3a5bfd4466078c3a9565f0f0926a675d0d324b
tomalbrc <tomalbrc@me.com>
2024-04-09 21:05:20 +0200

    dont mess with texture names. remove unused default transform. 1.1.5

--- src/main/java/de/tomalbrc/bil/file/extra/BbResourcePackGenerator.java
+++ src/main/java/de/tomalbrc/bil/file/extra/BbResourcePackGenerator.java
@@ -9,22 +9,16 @@ import de.tomalbrc.bil.json.ElementSerializer;
 import de.tomalbrc.bil.json.FaceSerializer;
 import de.tomalbrc.bil.json.JSON;
 import de.tomalbrc.bil.util.ResourcePackUtil;
-import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import net.minecraft.resources.ResourceLocation;
-import org.joml.Vector3f;
 
 import java.util.Base64;
 import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 
 public class BbResourcePackGenerator {
     static Gson gson = JSON.GENERIC_BUILDER
             .registerTypeAdapter(BbFace.class, new FaceSerializer())
             .registerTypeAdapter(BbElement.class, new ElementSerializer())
             .create();
-
-    static ResourcePackItemModel.DisplayTransform DEFAULT_TRANSFORM = new ResourcePackItemModel.DisplayTransform(new Vector3f(0,180,0), null, null); // default BIL model transform
 
     static String BASE64_PNG_PREFIX = "data:image/png;base64,";
     static String MODEL_DIR = ":assets/bil/models/item/";
@@ -40,7 +34,7 @@ public class BbResourcePackGenerator {
     public static void makeTextures(BbModel model, Collection<BbTexture> textures) {
         for (BbTexture texture : textures) {
             byte[] texData = Base64.getDecoder().decode(texture.source.replace(BASE64_PNG_PREFIX, ""));
-            ResourcePackUtil.add(new ResourceLocation(TEXTURE_DIR + model.modelIdentifier + "/" + texture.name + (texture.name.endsWith(".png") ? "":".png")), texData);
+            ResourcePackUtil.add(new ResourceLocation(TEXTURE_DIR + model.modelIdentifier + "/" + texture.name + ".png"), texData);
         }
     }
 }

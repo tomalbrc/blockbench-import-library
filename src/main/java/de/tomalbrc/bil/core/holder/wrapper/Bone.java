f2e3de3c55a7d020eef0c669207169959a997b65
tomalbrc <tomalbrc@me.com>
2024-10-28 12:46:30 +0100

    detect children of head nodes as head rotating

--- src/main/java/de/tomalbrc/bil/core/holder/wrapper/Bone.java
+++ src/main/java/de/tomalbrc/bil/core/holder/wrapper/Bone.java
@@ -6,19 +6,29 @@ import eu.pb4.polymer.virtualentity.api.elements.ItemDisplayElement;
 import eu.pb4.polymer.virtualentity.api.tracker.DisplayTrackedData;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.component.CustomModelData;
 import net.minecraft.world.item.component.DyedItemColor;
+import org.jetbrains.annotations.NotNull;
 
 public class Bone extends DisplayWrapper<ItemDisplayElement> {
     private final ItemStack item;
     private boolean invisible;
 
     public static Bone of(ItemDisplayElement element, Node node, Pose defaultPose, boolean isHead) {
-        return new Bone(element, node, defaultPose, isHead);
+        return Bone.of(element, node, defaultPose, isHead);
     }
 
-    public static Bone of(ItemDisplayElement element, Node node, Pose defaultPose) {
-        return new Bone(element, node, defaultPose, node.name().startsWith("head"));
+    public static Bone of(ItemDisplayElement element, @NotNull Node node, Pose defaultPose) {
+        Node current = node;
+        boolean head = false;
+        while (current != null) {
+            if (current.name().startsWith("head")) {
+                head = true;
+                break;
+            }
+            current = current.parent();
+        }
+
+        return new Bone(element, node, defaultPose, head);
     }
 
     protected Bone(ItemDisplayElement element, Node node, Pose defaultPose, boolean isHead) {

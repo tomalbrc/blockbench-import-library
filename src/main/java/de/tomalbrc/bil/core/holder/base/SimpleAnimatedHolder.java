51850a3dff426f45fa1b24d229351f18a9372552
tomalbrc <tomalbrc@me.com>
2025-05-30 00:41:09 +0200

    deprecate PositionedHolder, add SimpleAnimatedHolder

--- src/main/java/de/tomalbrc/bil/core/holder/base/SimpleAnimatedHolder.java
+++ src/main/java/de/tomalbrc/bil/core/holder/base/SimpleAnimatedHolder.java
@@ -1,27 +1,19 @@
-package de.tomalbrc.bil.core.holder.positioned;
+package de.tomalbrc.bil.core.holder.base;
 
-import de.tomalbrc.bil.core.holder.base.AbstractAnimationHolder;
 import de.tomalbrc.bil.core.model.Model;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.phys.Vec2;
-import net.minecraft.world.phys.Vec3;
 
-@Deprecated(forRemoval = true)
-public class PositionedHolder extends AbstractAnimationHolder {
-    public PositionedHolder(ServerLevel level, Vec3 pos, Model model) {
-        super(model, level);
-        this.currentPos = pos;
-    }
-
-    public PositionedHolder(ServerLevel level, Model model) {
+public class SimpleAnimatedHolder extends AbstractAnimationHolder {
+    protected SimpleAnimatedHolder(Model model, ServerLevel level) {
         super(model, level);
     }
 
     @Override
     public CommandSourceStack createCommandSourceStack() {
-        String name = String.format("PositionedHolder[%.1f, %.1f, %.1f]", this.getPos().x, this.getPos().y, this.getPos().z);
+        String name = String.format("SimpleAnimatedHolder[%.1f, %.1f, %.1f]", this.getPos().x, this.getPos().y, this.getPos().z);
         return new CommandSourceStack(
                 this.getServer(),
                 this.getPos(),

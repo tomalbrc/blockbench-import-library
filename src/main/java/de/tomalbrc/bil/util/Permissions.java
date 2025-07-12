46c35fb752d5fd62a17d19557ecfd98b8dd297d8
tomalbrc <tomalbrc@me.com>
2025-07-13 00:56:59 +0200

    fix: Permissions check, 1.5.3

--- src/main/java/de/tomalbrc/bil/util/Permissions.java
+++ src/main/java/de/tomalbrc/bil/util/Permissions.java
@@ -7,7 +7,7 @@ import java.util.function.Predicate;
 
 public class Permissions {
     public static boolean check(ServerPlayer player, String node, int defaultCheck) {
-        return Permissions.check(player, node, defaultCheck);
+        return me.lucko.fabric.api.permissions.v0.Permissions.check(player, node, defaultCheck);
     }
 
     public static Predicate<CommandSourceStack> require(String node, int fallbackLevel) {
